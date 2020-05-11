ee64cc29e6f6a3781c6f664f8d979ec8eaf23933
bcolloran <brendan@colloran.net>
2020-05-11 16:37:46 -0700

    set up dogfight_game as a module

---
+++ dogfight_game/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="dogfight_game",
+    version="0.1",
+    #   description='The dogfight_game joke in the world',
+    #   url='http://github.com/storborg/dogfight_game',
+    #   author='Flying Circus',
+    #   author_email='flyingcircus@example.com',
+    #   license='MIT',
+    packages=["dogfight_game"],
+    zip_safe=False,
+)
+
