7e496221462ed20672713ae03ef3572ffe3bf323
bcolloran <brendan@colloran.net>
2020-05-22 15:49:04 -0700

    jax_nn_utils working, 1d example of pytorch and jax give comparable results

--- jax/jax_nn_utils.py
+++ jax/jax_nn_utils.py
@@ -7,6 +7,7 @@ from jax import random
 from jax.ops import index, index_add, index_update
 
 import matplotlib.pyplot as plt
+from IPython.display import display, clear_output
 
 
 @jit
@@ -79,6 +80,13 @@ def init_network_params_He_offset(sizes):
     ]
 
 
+def init_network_params_zeros(sizes):
+    return [
+        (numpy.zeros((n, m)), numpy.zeros((n, 1)))
+        for m, n in zip(sizes[:-1], sizes[1:])
+    ]
+
+
 ### PREDICTION AND OPTIMIZATION
 
 
@@ -115,26 +123,162 @@ def update_normalize_grad(params, data, LR):
 
 
 def make_data_set(target_fn, target_dim, batch_size):
-    X = target_dim * numpy.random.rand(target_dim, batch_size) - 1
+    X = 2 * numpy.random.rand(target_dim, batch_size) - 1
     return [(X[:, i : i + 1], target_fn(X[:, i : i + 1])) for i in range(batch_size)]
 
 
 ### MODEL FITTER
 
 
-def plot_1d_model_fit(predict, params, target, Xs=None):
-    if Xs is None:
-        Xs = numpy.linspace(-1, 1, 1000)
-    # plt.plot(Xs, predict(params, x).item() for x in Xs])
-    plt.plot(Xs, predict(params, Xs.reshape(1, -1)).flatten())
-    plt.plot(Xs, target(Xs))
-    plt.show()
+class Plotter1d(object):
+    def __init__(self, target_fn, Xs=None, jupyter=True):
+        # self.predict = predict
+        self.target_fn = target_fn
+
+        if Xs is None:
+            self.Xs = numpy.linspace(-1, 1, 100)
+
+        fig, (axFit, axLoss) = plt.subplots(1, 2, figsize=(20, 5))
+
+        self.fig = fig
+        self.axFit = axFit
+        self.axLoss = axLoss
+        self.jupyter = True if jupyter else False
+
+    def update_plot(self, params, loss_list, batch, Xs=None):
+        if loss_list == []:
+            loss_list = [0]
+        self.fig.suptitle(f"batch {batch}, loss {loss_list[-1]}", fontsize=14)
+
+        self.axFit.clear()
+        self.axFit.plot(self.Xs, predict(params, self.Xs.reshape(1, -1)).flatten())
+        self.axFit.plot(self.Xs, self.target_fn(self.Xs))
+
+        self.axLoss.clear()
+        self.axLoss.plot(loss_list)
+        self.axLoss.set_yscale("log")
+        self.axLoss.set_yscale("log")
+
+        if self.jupyter:
+            clear_output(wait=True)
+            # plt.show()
+
+            display(self.fig)
+            # clear_output(wait=True)
+            # plt.show()
 
 
 def fit_model(
-    LR_0=0.1,
+    LR=0.001,
     LR_min=0.001,
-    decay=0.99,
+    decay=1,
+    epoch_size=100,
+    num_epochs=10,
+    batch_size=64,
+    target_fn=lambda x: x,
+    target_fn_dim=1,
+    layers=[None, 80, 40, 20, 1],
+    layer_initializer=init_network_params_He,
+    params=None,
+    plot_fn=None,
+):
+    LR_0 = LR
+    layers[0] = target_fn_dim
+
+    loss_list = []
+    param_list = []
+    if params is None:
+        params = layer_initializer(layers)
+
+    if plot_fn is not None:
+        epoch = 0
+        plot_fn(params, loss_list, epoch)
+
+    for i in range(epoch_size * num_epochs):
+        data = make_data_set(target_fn, target_fn_dim, batch_size)
+
+        l = loss(params, data).item()
+        loss_list.append(l)
+        if np.isnan(l):
+            print("loss is nan")
+            break
+
+        LR = max(LR_0 * decay ** i, LR_min)
+
+        params = update(params, data, LR)
+        print(f"batch {i}, loss {l}, LR {LR}", end="\r")
+
+        if (i + 1) % epoch_size == 0:
+            # print("epoch")
+            param_list.append(params)
+            if plot_fn is not None:
+                # print("should plot")
+                plot_fn(params, loss_list, i)
+
+    return param_list, loss_list
+
+
+# @jit
+def update_adam(params, data, ms, vs, t, LR, beta1=0.9, beta2=0.99, eps=1e-8):
+    # https://towardsdatascience.com/adam-latest-trends-in-deep-learning-optimization-6be9a291375c
+    grads = grad(loss)(params, data)
+
+    ms = [
+        (beta1 * m_w + (1 - beta1) * dw, beta1 * m_b + (1 - beta1) * db)
+        for (m_w, m_b), (dw, db) in zip(ms, grads)
+    ]
+
+    vs = [
+        (beta2 * v_w + (1 - beta2) * dw ** 2, beta2 * v_b + (1 - beta2) * db ** 2)
+        for (v_w, v_b), (dw, db) in zip(vs, grads)
+    ]
+
+    t = t + 1
+    # param_updates = [
+    #     (
+    #         LR * (m_w / (1 - beta1 ** t) / (v_w / (np.sqrt(1 - beta2 ** t) + eps))),
+    #         LR * (m_b / (1 - beta1 ** t) / (v_b / (np.sqrt(1 - beta2 ** t) + eps))),
+    #     )
+    #     for (m_w, m_b), (v_w, v_b) in zip(ms, vs)
+    # ]
+    # print("param_updates", param_updates)
+    # print("param_updates", (param_updates[0][0][0, 0].item()))
+    mHats = [(m_w / (1 - beta1 ** t), m_b / (1 - beta1 ** t)) for (m_w, m_b) in ms]
+
+    vHats = [(v_w / (1 - beta2 ** t), v_b / (1 - beta2 ** t)) for (v_w, v_b) in vs]
+
+    param_updates = [
+        (LR * mHat_w / (np.sqrt(vHat_w) + eps), LR * mHat_b / (np.sqrt(vHat_b) + eps),)
+        for (mHat_w, mHat_b), (vHat_w, vHat_b) in zip(mHats, vHats)
+    ]
+
+    # for (w_pu, b_pu) in param_updates:
+    #     if np.sum(w_pu ** 2) == 0:
+    #         print("np.sum(w_pu**2)==0")
+
+    #     if np.sum(b_pu ** 2) == 0:
+    #         print("np.sum(b_pu**2)==0")
+
+    #     if numpy.isnan(np.sum(w_pu ** 2)):
+    #         print("numpy.isnan(ip.sum(w_pu**2)")
+
+    #     if numpy.isnan(np.sum(b_pu ** 2)):
+    #         print("numpy.isnan(np.sum(b_pu**2)")
+    # vHatss = [
+    #     (beta2 * v_w + (1 - beta2) * dw ** 2, beta2 * v_b + (1 - beta2) * db ** 2)
+    #     for (v_w, v_b), (dw, db) in zip(vs, grads)
+    # ]
+    return (
+        [(w - w_pu, b - b_pu) for (w, b), (w_pu, b_pu) in zip(params, param_updates)],
+        ms,
+        vs,
+    )
+
+
+def fit_model_adam(
+    LR=0.0001,
+    # LR_min=0.001,
+    # decay=0.99,
     epoch_size=100,
     num_epochs=10,
     batch_size=64,
@@ -152,8 +296,12 @@ def fit_model(
     if params is None:
         params = layer_initializer(layers)
 
+    ms = init_network_params_zeros(layers)
+    vs = init_network_params_zeros(layers)
+
     if plot_fn is not None:
-        plot_fn(predict, params, target_fn)
+        epoch = 0
+        plot_fn(params, loss_list, epoch)
 
     for i in range(epoch_size * num_epochs):
         data = make_data_set(target_fn, target_fn_dim, batch_size)
@@ -164,14 +312,14 @@ def fit_model(
             print("loss is nan")
             break
 
-        LR = max(LR_0 * decay ** i, LR_min)
+        # LR = max(LR_0 * decay ** i, LR_min)
 
-        params = update(params, data, LR)
-        print(f"epoch {i}, loss {l}, LR {LR}", end="\r")
+        params, ms, vs = update_adam(params, data, ms, vs, i, LR)
+        # print(f"epoch {i}, loss {l}, LR {LR}", end="\r")
 
-        if i % epoch_size == 0:
+        if (i + 1) % epoch_size == 0:
             param_list.append(params)
             if plot_fn is not None:
-                plot_fn(predict, params, target_fn)
+                plot_fn(params, loss_list, i)
 
     return param_list, loss_list
